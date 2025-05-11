61c1f43d341f6445d0f27d2205a586f09de21c29
jty233 <3023631063@qq.com>
2025-05-11 20:57:30 +0800

    finish gpt2 kernal

--- include/gpt2.h
+++ include/gpt2.h
@@ -1,14 +1,32 @@
 #pragma once
+#include "gpt2_block.h"
 #include "model_parse.h"
+#include "tensor.h"
 #include <string>
+#include <vector>
 
 class GPT2 {
 public:
     GPT2(const std::string& model_path) : parser(model_path) {
+        for (int i = 0; i < num_blocks; i++) {
+            blocks.emplace_back(parser, "h." + std::to_string(i), num_head, hidden_dim);
+        }
+    }
 
+    Tensor<float> forward(Tensor<float> input) {
+        for (auto& block : blocks) {
+            input = block.forward(input);
+        }
+        return input;
     }
 
 private:
 
     ModelParse parser;
+    std::vector<GPT2Block> blocks;
+
+    int num_blocks = 12;
+    int num_head = 12;
+    int hidden_dim = 768;
+
 };
---
+++ include/gpt2_block.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "model_parse.h"
+#include "module.h"
+#include "modules/layer_norm.h"
+#include "modules/linear.h"
+#include "modules/multi_head_attention.h"
+#include "tensor.h"
+#include <string>
+struct GPT2Block {
+    GPT2Block(ModelParse& parser, const std::string& name, int num_head, int hidden_dim) : mha(num_head, hidden_dim, true) {
+        ln1.init(parser, name + "ln_1");
+        ln2.init(parser, name + "ln_2");
+        mha.in_proj.init(parser, name + "attn.c_attn", true);
+        mha.out_proj.init(parser, name + "attn.c_proj", true);
+        fc.init(parser, name + "mlp.c_fc", true);
+        proj.init(parser, name + "mlp.c_proj", true);
+
+    }
+
+    Tensor<float> forward(const Tensor<float>& input) {
+        Tensor<float> x = ln1.forward(input);
+        x = mha.forward(x);
+        x = x + input;
+        Tensor<float> out = ln2.forward(x);
+        out = fc.forward(out);
+        out = relu(out);
+        out = proj.forward(out);
+        return out + x;
+    }
+
+    LayerNorm<float> ln1, ln2;
+    MultiHeadAttention<float> mha;
+    Linear<float> fc, proj;
+};
