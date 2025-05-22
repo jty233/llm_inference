e399b206bc44569f3c9be9eeb80f065c58215429
jty233 <3023631063@qq.com>
2025-05-22 18:42:32 +0800

    parse bf16

--- src/model_parse.cpp
+++ src/model_parse.cpp
@@ -1,5 +1,9 @@
 #include "model_parse.h"
 #include "tensor.h"
+#include <cstdint>
+#include <cstring>
+#include <stdexcept>
+#include <vector>
 
 using namespace std;
 
@@ -14,13 +18,28 @@ void ModelParse::parse() {
 
 Tensor<float> ModelParse::getTensor(const std::string& name) {
     auto info = model_json[name];
-    assert(info["dtype"] == "F32");
     std::vector<int> shape = info["shape"];
     std::vector<long long> offset = info["data_offsets"];
     int len = offset[1] - offset[0];
     fin.seekg(offset[0] + 8 + json_length, ios::beg);
-    std::vector<float> val(len / sizeof(float));
-    read(val.data(), len);
-    return {std::move(val), std::move(shape)};
+    if (info["dtype"] == "F32") {
+        std::vector<float> val(len / sizeof(float));
+        read(val.data(), len);
+        return {std::move(val), std::move(shape)};
+    }
+    else if (info["dtype"] == "BF16") {
+        int num = len / 2;
+        std::vector<uint16_t> val(num);
+        read(val.data(), len);
+        vector<float> real_val(num);
+        for (int i = 0; i < num; i++) {
+            uint32_t temp = static_cast<uint32_t>(val[i]) << 16;
+            memcpy(real_val.data() + i, &temp, 4);
+        }
+        return {std::move(real_val), std::move(shape)};
+    }
+    else {
+        throw runtime_error("name error in model parse");
+    }
 }
 
